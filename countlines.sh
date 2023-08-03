e8664bda0cc606941df8c6747f9b8479a88ddd55
Peddinti <ec2-user@ip-172-31-44-65.ap-south-1.compute.internal>
2023-08-03 06:00:43 +0000

    adding countlines.sh

---
+++ countlines.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+echo "Enter the Name"
+
+read Name
+
+Lines=0
+
+if [ -f "$Name" ];
+
+then
+Lines= ` wc -l $Name `
+
+cat $Name
+
+echo "$Name has $Lines lines"
+
+else
+
+ls -p | grep -v "/$"
+
+fi
